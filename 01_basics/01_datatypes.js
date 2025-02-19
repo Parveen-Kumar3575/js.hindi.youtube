a9f74f3cdfaa7d03e9fb0f04470a94ba9915fc60
Parveen-Kumar3575 <parveen.verma3575@gmail.com>
2025-02-19 19:05:26 +0000

    datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+"strict type"; //treat all code as a newer version 
+
+let name = "Parveen"
+let age = 20
+let status = true
+let weather = null // null is a standalone value
+// symbol is also a unique dataype which is mainly use in javascript
+let xyz;
+// alert(3+3) 
+console.log(typeof name);
+console.log(typeof weather);
+console.log(typeof xyz);
+console.log(status);
