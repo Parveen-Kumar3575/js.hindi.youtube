ae8015188e93f5ab980fc0b44d8841436e308068
Parveen-Kumar3575 <parveen.verma3575@gmail.com>
2025-02-20 19:18:13 +0000

    data conversion done

---
+++ 01_basics/01_dataConversion.js
@@ -0,0 +1,10 @@
+let a = "10sbc";
+let data = Number(a);
+
+let bool = 0; // value khali ni honi chaiye jaise " " or 0 or empty etc.
+let isBoolean = Boolean(bool);
+console.log(isBoolean);
+console.log(typeof(a));
+console.log(typeof data);
+console.log(a);
+console.log(data);
