8bf6503e6a5e7e038c0006baa5553330c883e0e7
Parveen-Kumar3575 <parveen.verma3575@gmail.com>
2025-03-25 16:05:13 +0000

    destructure object ,arrays  done

--- 02_basics/06_objects.js
+++ 02_basics/06_objects.js
@@ -7,23 +7,35 @@ let obj = {
     School : "Gyandeep"
 }
 
-//Suppose if i want to acces School property then i can acces it as obj.School
-//but if we have to print it again and again we can write store it in variable and then print it 
-//we destructure the object;
-//Syntax of destructuring : 
-// let {propertyname} = objectName
-//let {propertyname : newpropertyName[optional]} = objectName
-//Generally, we use it during API data fetching where data comes in json (key,value pair) or array of objects ,before it use to come in XML
+/*
+Suppose if i want to acces School property then i can acces it as obj.School
+but if we have to print it again and again we can write store it in variable and then print it 
+we destructure the object;
+Syntax of destructuring : 
+let {propertyname} = objectName
+let {propertyname : newpropertyName[optional]} = objectName
+Generally, we use it during API data fetching where data comes in json (key,value pair) or array of objects ,before it use to come in XML
+
+*/
+
 
 let {School : newSchoolName} = obj;
 //This is quivalent to newSchoolName : obj.School
 // console.log(School);
 console.log(newSchoolName);
+
+
 //Nested objects destructuring 
 let user = {info : {firstName : "Parveen",lastName : "Kumar"}};
 let {info : {firstName : fname}} = user;
 console.log(fname);
-let {info["lastName"] : Lname} = user;
+/*
+let { info["firstName"]: fname } = obj; is incorrect
+info["firstName"] is not a valid destructuring pattern because destructuring works with property names directly, not expressions.
+
+*/
+
+
 
 //Basically it is oppsoite of assignment for ex-
 let arr = [10,20,30];  //the way we assign 
