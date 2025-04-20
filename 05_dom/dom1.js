94809d1e0992b38d703b4cec8210fee2abb892a7
Parveen-Kumar3575 <parveen.verma3575@gmail.com>
2025-04-20 12:34:43 +0000

    dom intro done

---
+++ 05_dom/dom1.js
@@ -0,0 +1,25 @@
+/*
+dom ek programming interface hai for document objects jo allow krta hai webpage k content ya structure k sath interact , access ya modify krne ka
+
+- In simple terms:
+ 1) The HTML page you write is converted by the browser into a tree-like structure.
+ 2) This structure is called the DOM.
+ 3) Each part of the HTML (like a tag, attribute, or text) becomes a node in this tree.
+
+
+ sabse pehle window fir
+ Document
+ └── html
+      └── body
+           ├── h1 → "Hello"
+           └── p  → "Welcome to my page"
+
+node - ek generic term hai , sab kuch dom me node hai
+element - ye bhi trah ka node hai jo ki html element ko kehte hai
+
+
+console.dir(obj) - ye object k saare methods properties etc detail me show krega
+
+*/
+
+
